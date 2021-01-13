1c64bf34b42db68dc55b14ab31698ea7734110ca
FarnaZ <74882606+Farnaaaz@users.noreply.github.com>
2021-01-12 23:24:16 -0500

    Create MultiplicationTable.java

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+//This code print the multiplication table 1X100
+
+public static void main(String[] arg){
+
+  for (i=1 ; i<=10 ; i++)
+  {
+    for (j=1; j<=10 ; j++)
+    {
+      System.out.print(i * j + "\t");
+    }
+    System.out.println();
+  }
+}
+
