f178bc9d70b18e4d0e6bbb68112afd6f140d59bb
FarnaZ <74882606+Farnaaaz@users.noreply.github.com>
2021-05-15 11:54:49 -0400

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+x = range(1, 101)
+
+for y in x:
+ if (y % 3 == 0 and y % 5 == 0):
+  print ("Fizz Buzz")
+ elif y % 5 == 0:
+  print ("Buzz")
+ elif y % 3 == 0:
+  print ("Fizz")
+ else:
+  print (y)
+© 2021 GitHub, Inc.
